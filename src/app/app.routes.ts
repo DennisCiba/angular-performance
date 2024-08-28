f29b29b90b6e8112381a30aa33e52672b1853b48
Dennis Ciba <dennis.ciba@freenet.ag>
2024-08-28 17:58:07 +0200

    fix: Lazy Loading deaktivieren

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,12 @@
 import { Routes } from '@angular/router';
 import { ChangedetectionComponent } from './components/changedetection/changedetection.component';
+import { BundleComponent } from './components/bundle/bundle.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/change-detection', pathMatch: 'full' },
   { path: 'change-detection', component: ChangedetectionComponent },
   {
     path: 'bundle',
-    loadComponent: () =>
-      import('./components/bundle/bundle.component').then(
-        m => m.BundleComponent
-      ),
+    component: BundleComponent,
   },
 ];
